d76b4aab730ce5f3f56fcf2d9abc09e5731a579f
shashiteegulla <teegullashashikumar@gmail.com>
2025-08-10 10:34:44 -0400

    Code refactor

---
+++ src/graphql/user/resolvers.ts
@@ -0,0 +1,9 @@
+const queries = {};
+
+const mutations = {
+    createUser: (_:any, {}:{}) => {
+        return "randomId"
+    }
+};
+
+export const resolvers = { queries, mutations }
--- src/index.ts
+++ src/index.ts
@@ -6,59 +6,59 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import {ApolloServerPluginLandingPageGraphQLPlayground} from '@apollo/server-plugin-landing-page-graphql-playground'
 import {prismaClient} from './lib/db.js';
+import createApolloGraphqlServer from './graphql/index.js';
 
 // Type definitions (schema)
-const typeDefs = `
-  type Query {
-    hello: String
-    say(name: String): String
-  }
-  type Mutation {
-    createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
-  }  
-`;
+// const typeDefs = `
+//   type Query {
+//     hello: String
+//     say(name: String): String
+//   }
+//   type Mutation {
+//     createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+//   }  
+// `;
 
-// Resolvers
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-    say: (_:any, {name}:{name:String}) => `Hey ${name}, How are you?`
-  },
-  Mutation: {
-    createUser: async (_:any, {firstName, lastName, email, password}: 
-      {firstName:string; lastName:string; email: string; password: string}) => {
-        await prismaClient.user.create({
-          data:{
-            email,
-            firstName,
-            lastName,
-            password,
-            salt: 'random_salt'
-          }
-        });
-        return true;
-      }
-  }
-};
+// // Resolvers 
+// const resolvers = {
+//   Query: {
+//     hello: () => 'Hello world!',
+//     say: (_:any, {name}:{name:String}) => `Hey ${name}, How are you?`
+//   },
+//   Mutation: {
+//     createUser: async (_:any, {firstName, lastName, email, password}: 
+//       {firstName:string; lastName:string; email: string; password: string}) => {
+//         await prismaClient.user.create({
+//           data:{
+//             email,
+//             firstName,
+//             lastName,
+//             password,
+//             salt: 'random_salt'
+//           }
+//         });
+//         return true;
+//       }
+//   }
+// };
 
 // Create Apollo Server with Playground plugin
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  introspection: true, // 👈 Needed in production to allow introspection
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()], // 👈 Enable GUI
-});
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   introspection: true, // 👈 Needed in production to allow introspection
+//   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()], // 👈 Enable GUI
+// });
 
 async function start() {
   const app = express();
 
-  await server.start();
-
+  //const server = await createApolloGraphqlServer();
   app.use(
     '/graphql',
     cors(),
     bodyParser.json(),
-    expressMiddleware(server)
+    expressMiddleware(await createApolloGraphqlServer())
   );
 
   app.listen(8000, () => {
